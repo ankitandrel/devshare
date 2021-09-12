5705cba565d2939c86b2bcd0ec5fb9fc431d70a1
Ankit <heyankitandrel@gmail.com>
2021-09-12 22:35:01 +0530

    email issue

--- devshare/settings.py
+++ devshare/settings.py
@@ -168,8 +168,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
 
 # Twilio SendGrid
 EMAIL_HOST = 'smtp.sendgrid.net'
